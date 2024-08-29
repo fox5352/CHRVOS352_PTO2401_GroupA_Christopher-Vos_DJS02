6c0db5ef1c9a279c4ff35fed91289d32acee747f
Christopher <voschristopher53@gmail.com>
2024-08-29 14:04:45 +0200

    Providing anything that is not a number should crash the program. User story completed

--- scripts.js
+++ scripts.js
@@ -5,18 +5,27 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  if (divider.length > 0 && dividend.length > 0) {
+  
+  if (isNaN(Number(dividend)) || isNaN(Number(divider))) {
+    document.body.innerText = "Something critical went wrong. Please reload the page"
+    throw new Error("failed to parse number");
+
+  }else if (divider.length > 0 && dividend.length > 0) {
     try {
       if (dividend == 0 | divider == 0) {
         throw new RangeError("zero divider");
+
       }else {
         result.innerText = Math.floor(dividend / divider);
+
       }
     } catch (error) {
       console.error(error);
       result.innerText = "Division not performed. Invalid number provided. Try again"
     }
+
   }else {
     result.innerText = "Division not performed. Both values are required in inputs. Try again"
+
   }
 });
