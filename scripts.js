51df708145e64d733e2aefcf64e009aff70c05e6
Christopher <voschristopher53@gmail.com>
2024-08-29 13:31:51 +0200

    Division not performed. Both values are required in inputs. Try again

--- scripts.js
+++ scripts.js
@@ -5,5 +5,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider);
+  if (divider.length > 0 && dividend.length > 0) {
+    result.innerText = Math.floor(dividend / divider);
+  }else {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again"
+  }
 });
